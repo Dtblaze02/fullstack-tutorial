de2b3ce19e2513434800e60f6ebb57d9263ed1ce
Dtblaze02 <sadiqaliyu89@gmail.com>
2023-06-19 11:28:40 +0100

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -30,38 +30,9 @@
       <div class="form-group">
         <button type="submit">Register</button>
       </div>
-      <a href="../index.php">Login</a>
+      <p>Already have an account? <a href="../index.php">Login Now</a> </p>
     </form>
   </div>
-
-  <!-- <div class="auth-forms mt-5">
-    <h2 class="text-center">Registration</h2>
-    <form id="registrationForm" onsubmit="validateForm(event)" action="../auth/register.php" method="POST">
-      <div class="row">
-        <div class="col-md-6 offset-md-3">
-          <div class="mb-3">
-            <label for="username" class="form-label">Username:</label>
-            <input type="text" id="username" name="username" class="form-control" placeholder="Enter a username">
-            <span class="error-message text-danger" id="usernameError"></span>
-          </div>
-          <div class="mb-3">
-            <label for="email" class="form-label">Email:</label>
-            <input type="email" id="email" name="email" class="form-control" placeholder="Enter your email">
-            <span class="error-message text-danger" id="emailError"></span>
-          </div>
-          <div class="mb-3">
-            <label for="password" class="form-label">Password:</label>
-            <input type="password" id="password" name="password" class="form-control" placeholder="Enter a password">
-            <span class="error-message text-danger" id="passwordError"></span>
-          </div>
-          <div class="mb-3">
-            <button type="submit" class="btn btn-primary">Register</button>
-          </div>
-          <a href="../index.php">Login</a>
-        </div>
-      </div>
-    </form>
-  </div> -->
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/js/bootstrap.bundle.min.js"></script>
   <script>
