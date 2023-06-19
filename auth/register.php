97380fdbc16308f768363beee203c313df3a460a
Dtblaze02 <sadiqaliyu89@gmail.com>
2023-06-19 11:24:23 +0100

    Create register.php

---
+++ auth/register.php
@@ -0,0 +1,27 @@
+<?php
+// register.php
+
+// Retrieve form data
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Validate the data (you can add more validation if needed)
+if (empty($username) || empty($email) || empty($password)) {
+    echo "Please fill in all fields.";
+} else {
+    require('db_conn.php');
+    
+    // Perform the registration
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '" . md5($password) . "')";
+    
+    if ($conn->query($sql) === TRUE) {
+        header('Location: ../index.php');
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    // Close the database connection
+    $conn->close();
+}
+?>
